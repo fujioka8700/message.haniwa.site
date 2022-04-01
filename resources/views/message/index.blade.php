e870bfa5e4ce5c1a80bcd33e41c3a0073c6da47c
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-04-01 10:04:18 +0900

    [fix] ログイン後のルーティング調整

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -5,6 +5,17 @@
   <meta charset="UTF-8">
 </head>
 <body>
+    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </div>
     {{-- @php
         foreach ($category->messages as $message) {
             echo "{$message->id}:{$message->title}<br>";
